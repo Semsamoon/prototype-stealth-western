1f16f2ad09ea8e93f28ec48d764e8d89020057f7
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-08-14 09:25:12 +0300

    refactor: move character fsm to player controller

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -2,13 +2,6 @@ using UnityEngine;
 
 public abstract class CharacterController : MonoBehaviour
 {
-    protected enum State
-    {
-        Moving,
-        Stealth
-    }
-
-    protected State _currentState;
     protected CharacterMovement _movement;
     protected Animator _animator;
 
@@ -16,36 +9,5 @@ public abstract class CharacterController : MonoBehaviour
     {
         _movement = GetComponent<CharacterMovement>();
         _animator = GetComponent<Animator>();
-        _currentState = State.Stealth;
-    }
-
-    protected void UpdateState()
-    {
-        if (_currentState == State.Moving) HandleMovingState();
-        else HandleStealthState();
-    }
-
-    protected virtual void HandleMovingState()
-    {
-        if (_movement.IsMoving()) return;
-        TransitionToStealth();
-    }
-
-    protected virtual void HandleStealthState()
-    {
-        if (!_movement.IsMoving()) return;
-        TransitionToMoving();
-    }
-
-    protected void TransitionToMoving()
-    {
-        _currentState = State.Moving;
-        _animator.SetBool("IsMoving", true);
-    }
-
-    protected void TransitionToStealth()
-    {
-        _currentState = State.Stealth;
-        _animator.SetBool("IsMoving", false);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,13 @@ using UnityEngine.InputSystem;
 
 public sealed class PlayerController : CharacterController
 {
+    private enum State
+    {
+        Moving,
+        Stealth
+    }
+
+    private State _currentState;
     private PlayerInput _playerInput;
     private Vector2 _inputDirection;
 
@@ -10,6 +17,7 @@ public sealed class PlayerController : CharacterController
     {
         base.Awake();
         _playerInput = GetComponent<PlayerInput>();
+        _currentState = State.Stealth;
     }
 
     private void OnEnable()
@@ -30,6 +38,36 @@ public sealed class PlayerController : CharacterController
     {
         UpdateState();
         MoveCharacter();
+    }
+
+    private void UpdateState()
+    {
+        if (_currentState == State.Moving) HandleMovingState();
+        else HandleStealthState();
+    }
+
+    private void HandleMovingState()
+    {
+        if (_movement.IsMoving()) return;
+        TransitionToStealth();
+    }
+
+    private void HandleStealthState()
+    {
+        if (!_movement.IsMoving()) return;
+        TransitionToMoving();
+    }
+
+    private void TransitionToMoving()
+    {
+        _currentState = State.Moving;
+        _animator.SetBool("IsMoving", true);
+    }
+
+    private void TransitionToStealth()
+    {
+        _currentState = State.Stealth;
+        _animator.SetBool("IsMoving", false);
     }
 
     private void OnMove(InputAction.CallbackContext context)
