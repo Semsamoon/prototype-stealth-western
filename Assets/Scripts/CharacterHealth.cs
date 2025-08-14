9bd7faadf0285771d3c3c128dc13b74df31b407c
Semsamoon <94685404+Semsamoon@users.noreply.github.com>
2025-08-14 09:14:46 +0300

    feat(character): add character health

---
+++ Assets/Scripts/CharacterHealth.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CharacterHealth : MonoBehaviour
+{
+    protected bool _isAlive = true;
+
+    public virtual void TakeDamage()
+    {
+        if (!_isAlive) return;
+        _isAlive = false;
+        TriggerDeath();
+    }
+
+    protected virtual void TriggerDeath()
+    {
+        Debug.Log($"{gameObject.name} has died!");
+    }
+
+    public virtual bool IsAlive()
+    {
+        return _isAlive;
+    }
+}
